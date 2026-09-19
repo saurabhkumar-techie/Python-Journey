c238e792135a9343d95f9e9de4461d01e617c7d3
Saurabh kumar <saurabhkumar.techie@gmail.com>
2026-09-20 01:26:56 +0530

    Sunday Python Practice

---
+++ Python basics-practice sets/question-3.py
@@ -0,0 +1,6 @@
+name = "Saurabh"
+age = 19
+height = 160.4
+is_student = True
+
+print("The name of the student is " + name + ". He is " + str(age) + " years old. " + "He is " + str(height) + " cms tall. " + "Student: " + str(is_student))
